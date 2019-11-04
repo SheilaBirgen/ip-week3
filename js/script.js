80363ff3951e656fee74739dc38ac8f3fc5139a0
SheilaBirgen <jeronobergen@gmail.com>
2019-11-04 15:34:19 +0300

    add a form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
   $("#design-img").show();
   $("#design-img").click(function() {
@@ -24,14 +25,17 @@ $(document).ready(function() {
     $("#product-p").hide();
     $("#product-img").show();
   });
-
-  $("images").hover(function() {
-    $(this).css("opacity", "10");
-  });
-
-  $(".btnsend").click(function(event) {
-    event.preventDefault();
-    var user = document.getElementById("name").value;
-    alert("Hello there " + user + " your message has been received. Thank you");
-  });
+     $("#submission").submit(function(event){
+    var data_1 = $("input:first").val();
+    var data_2 = $("input#mail").val();
+    if(data_1 && data_2 != ""  ){
+      $(".message").text("Thank you " + data_1 + " Your message has been received");
+      $(".message").removeClass("error");
+      $(".message").addClass("success");
+    }else{
+      $(".message").text("Error!!! Please fill in all the details correctly");
+      $(".message").addClass("error");
+    }
+    event.preventDefault(); 
+  });     
 });
