e122bf0d98b69390f74a96c68e8d9e5323cf05f4
SheilaBirgen <jeronobergen@gmail.com>
2019-11-04 07:27:53 +0300

    add a background image

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
   $("#design-img").click(function() {
     $(this).hide(2000);
     $("#design").show(2000);
-  };
+  });
   $("#design").click(function() {
     $(this).hide(2000);
     $("#design-img").show(2000);
